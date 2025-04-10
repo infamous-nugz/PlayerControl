3a26559b8bbf06d2f2d97db90ccabb274314b54f
infamous_nugz <rosarioj6@roverkids.org>
2025-04-10 14:14:13 -0400

    plane

---
+++ PlaneProject/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject player;
+    public float speed = 20;
+    public float rotationSpeed;
+    public float verticalInput;
+
+    // Start is called before the first frame update
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
+    }
+}
